a14ec046b9b9aa8db82aa8687b236b94b1bb6854
dunzhixuan <dunzhixuan@vipkid.com.cn>
2019-04-29 15:44:36 +0800

    First commit

---
+++ app/src/main/java/com/zhixuan/app/fricircle/Fragment_Chat.java
@@ -0,0 +1,22 @@
+package com.zhixuan.app.fricircle;
+
+import com.dunzhixuan.app.libbase.baseui.MvpBaseFragment;
+import com.dunzhixuan.app.libbase.baseui.MvpBasePresenter;
+
+
+/**
+ * 作者： 敦志轩 on 2018/1/30.
+ * 邮箱： 997681136@qq.com
+ */
+
+public class Fragment_Chat extends MvpBaseFragment {
+    @Override
+    protected int setContentView() {
+        return R.layout.fragment_chat;
+    }
+
+    @Override
+    protected MvpBasePresenter createPresenter() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/zhixuan/app/fricircle/Fragment_Video.java
@@ -0,0 +1,21 @@
+package com.zhixuan.app.fricircle;
+
+import com.dunzhixuan.app.libbase.baseui.MvpBaseFragment;
+import com.dunzhixuan.app.libbase.baseui.MvpBasePresenter;
+
+/**
+ * 作者： 敦志轩 on 2018/1/30.
+ * 邮箱： 997681136@qq.com
+ */
+
+public class Fragment_Video extends MvpBaseFragment {
+    @Override
+    protected int setContentView() {
+        return R.layout.fragment_video;
+    }
+
+    @Override
+    protected MvpBasePresenter createPresenter() {
+        return null;
+    }
+}
